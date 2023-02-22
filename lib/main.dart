234c15e41e62d5f500c1a602e605899bb1192be3
Sayyidusy syauqi Al Ghiffari <sayyidusyalghiffari@gmail.com>
2023-02-22 11:29:59 +0700

    homescreen

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:dinotis/routes.dart';
+import 'package:dinotis/screens/splash/splash_screen.dart';
+import 'package:dinotis/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'kopi telu',
+      theme: theme(),
+      // home: SplashScreen(),
+      initialRoute: SplashScreen.routeName,
+      // routes: {
+      //   SplashScreen.routeName: (context) => SplashScreen(),
+      // },
+      routes: routes,
+    );
+  }
+}
